9ce6927a28f068bb1d007b461d1e6ac526fd7b15
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-01-23 16:18:44 -0300

    métodos setter que reatribue valores de propriedades existentes em um objeto

--- object/propriedades.js
+++ object/propriedades.js
@@ -55,4 +55,29 @@ let spaceship = {
   
   console.log(spaceship);
   
+  //Setter
+  const robot = {
+    _model: "1E78V2",
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors() {
+      if (typeof this._numOfSensors === "number") {
+        return this._numOfSensors;
+      } else {
+        return "Sensors are currently down.";
+      }
+    },
+    set numOfSensors(num) {
+      if (typeof num === "number" && num >= 0) {
+        this._numOfSensors = num;
+      } else {
+        console.log("Pass in a number that is greater than or equal to 0");
+      }
+    },
+  };
+  
+  //Use o numOfSensorsmétodo setter robotpara atribuir _numOfSensorsa 100.
+  robot.numOfSensors = 100;
+  
+  console.log(robot.numOfSensors);
   
